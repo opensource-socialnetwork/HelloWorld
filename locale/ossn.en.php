f6994322a22836299af29a4028674d00d47e9c8d
Arsalan Shah <manactric@gmail.com>
2023-09-22 19:16:28 +0500

    Create ossn.en.php

---
+++ locale/ossn.en.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   Open Source Social Network
+ * @author    Open Social Website Core Team <info@openteknik.com>
+ * @copyright (C) OpenTeknik LLC
+ * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
+ * @link      https://www.opensource-socialnetwork.org/
+ */
+ossn_register_languages('en', array(
+    	'lang:key:goes:here' => "Translation goes here!",
+));
